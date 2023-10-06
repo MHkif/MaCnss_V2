d304aeb0b3c15c207bec953804f8432a03551058
MHkif <malikhkif@gmail.com>
2023-10-06 10:52:09 +0200

    Create Controllers

---
+++ src/main/java/org/macnss/controllers/CompanyController.java
@@ -0,0 +1,15 @@
+package org.macnss.controllers;
+
+import org.macnss.Utils.PrintStatement;
+import org.macnss.Utils.UniqueCodeGenerator;
+import org.macnss.entity.ADocument;
+import org.macnss.entity.Employer;
+
+public class CompanyController extends Controller{
+
+        public void index() {
+        }
+
+
+
+}
---
+++ src/main/java/org/macnss/controllers/EmployerController.java
@@ -0,0 +1,29 @@
+package org.macnss.controllers;
+
+import org.macnss.Utils.PrintStatement;
+import org.macnss.Utils.UniqueCodeGenerator;
+import org.macnss.entity.*;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class EmployerController extends Controller{
+
+    public void create(){
+    
+    }
+     public void update(Employer employer){
+
+     }
+
+     public void deactivate(Employer employer){
+
+     }
+
+     public void showFolderHistory(){
+
+     }
+
+}
